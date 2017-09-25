d1d5bf8053909978264ad86b9d7e20ee92eadcf5
FAlkhanaizi1 <fatema.alkhanaizi@durham.ac.uk>
2017-09-25 09:44:01 +0100

    removed unused configs and updated some paths

--- scripts/close-all.sh
+++ scripts/close-all.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo '<----- closing kafka servers --->'
-${CONFLUENT_HOME}/bin/kafka-server-stop configs/kafka-server-00.properties &
-sleep 10 && ${CONFLUENT_HOME}/bin/kafka-server-stop configs/kafka-server-01.properties &
-sleep 10 && ${CONFLUENT_HOME}/bin/kafka-server-stop configs/kafka-server-02.properties &
-sleep 10 && ${CONFLUENT_HOME}/bin/kafka-server-stop configs/kafka-server-03.properties &
-sleep 10 && ${CONFLUENT_HOME}/bin/kafka-server-stop configs/kafka-server-04.properties &
+${CONFLUENT_HOME}/bin/kafka-server-stop ${DIR}/../configs/kafka-server-00.properties &
+sleep 10 && ${CONFLUENT_HOME}/bin/kafka-server-stop ${DIR}/../configs/kafka-server-01.properties &
+sleep 10 && ${CONFLUENT_HOME}/bin/kafka-server-stop ${DIR}/../configs/kafka-server-02.properties &
+sleep 10 && ${CONFLUENT_HOME}/bin/kafka-server-stop ${DIR}/../configs/kafka-server-03.properties &
+sleep 10 && ${CONFLUENT_HOME}/bin/kafka-server-stop ${DIR}/../configs/kafka-server-04.properties &
 
 echo '<----- closing schema registry --->'
 sleep 10 && ${CONFLUENT_HOME}/bin/schema-registry-stop ${CONFLUENT_HOME}/etc/schema-registry/schema-registry.properties &
