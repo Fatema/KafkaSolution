6be41184b8245e53b7a38c4b6bf4721e28a8d996
FAlkhanaizi1 <fatema.alkhanaizi@durham.ac.uk>
2017-09-25 14:52:26 +0100

    connectors seem to run better when they are running on their own and not in a script

--- scripts/start-connectors.sh
+++ scripts/start-connectors.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#create tables in a sqlite database and import data into it
-echo '<----- creating tables and importing data in sqlite --->'
-sqlite3 finance.db < ${DIR}/../database-scripts/finance.sql
-
-#create a keyspace and a table in cassandara
-echo '<----- creating keyspace and a table in cassandra --->'
-cqlsh -u 'cassandra' -p 'cassandra' -f ${DIR}/../database-scripts/HistoricalData.cql
-
-echo '<----- creating topics --->'
-kafka-topics --create --topic finance-sqlite-apple --zookeeper localhost:2181 --replication-factor 3 --partitions 3
-kafka-topics --create --topic finance-sqlite-ms --zookeeper localhost:2181 --replication-factor 3 --partitions 3
-kafka-topics --create --topic finance-sqlite-csco --zookeeper localhost:2181 --replication-factor 3 --partitions 3
-kafka-topics --create --topic finance-sqlite-apple-selected --zookeeper localhost:2181 --replication-factor 3 --partitions 3
-
 # add the sink or source to the connectors list / can use a POST request to http://localhost:8083/connectors for this
 echo '<----- adding sqlite-kafka source --->'
 java -jar ${DIR}/../libs/kafka-connect-cli-1.0-all.jar create finance-source-sqlite < ${DIR}/../configs/finance-source-sqlite.properties
@@ -24,6 +10,3 @@ echo '<----- adding cassandra-kafka sink --->'
 sleep 40 & java -jar ${DIR}/../libs/kafka-connect-cli-1.0-all.jar create finance-sink-cassandra < ${DIR}/../configs/finance-sink-cassandra.properties
 sleep 40 & java -jar ${DIR}/../libs/kafka-connect-cli-1.0-all.jar create finance-sink-cassandra-ms < ${DIR}/../configs/finance-sink-cassandra-ms.properties
 sleep 40 & java -jar ${DIR}/../libs/kafka-connect-cli-1.0-all.jar create finance-sink-cassandra-apple-selected < ${DIR}/../configs/finance-sink-cassandra-apple-selected.properties
-
-java -jar ${DIR}/../libs/kafka-connect-cli-1.0-all.jar run finance-source-sqlite < ${DIR}/../configs/finance-source-sqlite.properties
-sleep 40 & java -jar ${DIR}/../libs/kafka-connect-cli-1.0-all.jar run finance-source-sqlite-apple-selected < ${DIR}/../configs/finance-source-sqlite-apple-selected.properties
